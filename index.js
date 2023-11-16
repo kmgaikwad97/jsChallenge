7031eecd6892ea808d69f3fc73cbd417b9922a0c
kmgaikwad97 <kmgaikwad97@gmail.com>
2023-11-16 18:56:51 +0530

    changes

---
+++ index.js
@@ -0,0 +1,19 @@
+// https://codeguppy.com/blog/javascript-coding-challenges-for-beginners/index.html
+
+// #1: Print numbers from 1 to 10
+// for(i =1;i<=10;i++){
+//     console.log('val of i',i)
+// }
+
+// #2: Print the odd numbers less than 100
+// solution a
+// for(i=1;i<=10;i+=2){
+//     // console.log(i%2===0);
+//     console.log(i);
+// }
+// solution b
+// for(i=0;i<=10;i++){
+//     if(i%2!==0){
+//     console.log(i);
+// }
+// }
